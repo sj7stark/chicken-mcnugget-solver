8881a80e5fc999a3be68a372fb305a84135d52a7
sj7stark <72771546+sj7stark@users.noreply.github.com>
2023-07-03 16:17:17 -0400

    created entrypoint page

---
+++ Chicken_McNugget_Problem_Solver.py
@@ -0,0 +1,6 @@
+"""
+This file is the entrypoint for a multipage Streamlit app. This app allows users to setup and solve 
+a Chicken McNugget Problem. This entrypoint page contains a readme for the app. Other pages will contain
+a space for setting up and solving a problem, information regarding the problem, and more.
+"""
+
